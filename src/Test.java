d8bed889093e19d4a2e5631257b3bd6dd7a0586e
kanchanna <ramasamybalraj@gmail.com>
2017-09-24 23:00:04 -0400

    added Test

--- src/Test.java
+++ src/Test.java
@@ -6,12 +6,10 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
+
 		System.out.println("ttt");
 
-=======
-			printf("test");
->>>>>>> 33e6bcb1fb890b986c7d265c7e5d76ad926e88a0
 	}
 
 }
